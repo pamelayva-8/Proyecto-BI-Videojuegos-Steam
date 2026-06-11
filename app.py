413f839aca106c6b6b69cf752b55cb1077234ac1
pamelayva-8 <pamelayva@gmail.com>
2026-06-11 03:49:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # HEADER
@@ -33,4 +35,60 @@ st.write(df.head())
 
 st.markdown("##  Videojuegos steam")
 
+#Métricas
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.metric(
+        "Videojuegos",
+        len(df)
+    )
+
+with col2:
+    st.metric(
+        "Rating promedio",
+        round(df['rating'].mean(), 2)
+    )
+
+with col3:
+    st.metric(
+        "Precio promedio",
+        round(df['price'].mean(), 2)
+    )
+
+#Pregunta
+st.markdown("""
+### Pregunta de investigación
+
+¿Qué características influyen en que un videojuego tenga una calificación positiva?
+""")
+
+#Estadísticas descriptivas
+t.markdown("### Estadísticas descriptivas")
+
+st.dataframe(
+    df[
+        [
+            'rating',
+            'price',
+            'peak_ccu',
+            'owners_numeric'
+        ]
+    ].describe()
+)
+
+#Gráfica 1
+st.markdown("### Distribución del Rating")
+
+fig, ax = plt.subplots(figsize=(8,5))
+
+sns.histplot(
+    df['rating'],
+    bins=30,
+    kde=True,
+    ax=ax
+)
+
+ax.set_title("Distribución del Rating")
+
+st.pyplot(fig)
