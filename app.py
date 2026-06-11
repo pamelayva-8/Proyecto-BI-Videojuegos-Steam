46f836378d6e1438e49c6e2381ed5dff4adc84c3
pamelayva-8 <pamelayva@gmail.com>
2026-06-11 04:10:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -58,19 +58,19 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.metric(
         "Videojuegos",
-        len(df)
+        len(df_filtrado)
     )
 
 with col2:
     st.metric(
         "Rating promedio",
-        round(df['rating'].mean(), 2)
+        round(df_filtrado['rating'].mean(), 2)
     )
 
 with col3:
     st.metric(
         "Precio promedio",
-        round(df['price'].mean(), 2)
+        round(df_filtrado['price'].mean(), 2)
     )
 st.divider()
 
@@ -108,7 +108,7 @@ st.markdown("### Distribución del Rating")
 fig, ax = plt.subplots(figsize=(8,5))
 
 sns.histplot(
-    df['rating'],
+    df_filtrado['rating'],
     bins=30,
     kde=True,
     ax=ax
